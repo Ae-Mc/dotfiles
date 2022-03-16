a43803c1c7133c4450f11736a54a64ec3dc041cd
Ae-Mc <ae_mc@mail.ru>
2022-03-17 02:36:10 +0300

    feat: improve zsh config and other minor improvements

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,17 @@ else
 fi
 
 $mngr install git
+ln -s `pwd`/.gitconfig $HOME/.gitconfig
+
+$mngr install curl
 
 $mngr install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 zsh -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \${ZSH_CUSTOM:-\$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-cp `pwd`/.zshrc ~/.zshrc
-cp `pwd`/.p10k.zsh ~/.p10k.zsh
+rm ~/.zshrc
+rm ~/.p10k.zsh
+ln -s `pwd`/.zshrc $HOME/.zshrc
+ln -s `pwd`/.p10k.zsh $HOME/.p10k.zsh
 
 $mngr install autojump
 

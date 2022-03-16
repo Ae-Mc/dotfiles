2f11d0231ac6b76272f8e4e1840bf74e69ec5401
Ae-Mc <ae_mc@mail.ru>
2022-03-17 01:48:35 +0300

    feat: add base programs installation and zsh configuration

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+OS=$(uname)
+if [ $OS = "FreeBSD" ]; then
+    mngr=pkg
+else
+    echo "Unknown OS: $OS"
+    exit 1
+fi
+
+$mngr install git
+
+$mngr install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \${ZSH_CUSTOM:-\$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+cp `pwd`/.zshrc ~/.zshrc
+cp `pwd`/.p10k.zsh ~/.p10k.zsh
+
+$mngr install autojump
+
+$mngr install python310
+python3.10 -m ensurepip
+$mngr install curl
+$mngr install neovim
+$mngr install npm
+
